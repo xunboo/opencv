204421135c4ce5b224b537468f4b638e3bb05e6e
erwei <wnlvcq@gmail.com>
2019-10-01 10:22:12 -0700

    dcn should be 3 in order to work with OCL

--- modules/imgproc/src/color.cpp
+++ modules/imgproc/src/color.cpp
@@ -96,7 +96,7 @@ static bool ocl_cvtColor( InputArray _src, OutputArray _dst, int code, int dcn )
 
     case COLOR_YCrCb2BGR:
     case COLOR_YCrCb2RGB:
-        return oclCvtcolorYCrCb2BGR(_src, _dst, dcn, bidx);
+        return oclCvtcolorYCrCb2BGR(_src, _dst, 3, bidx);
 
     case COLOR_BGR2XYZ:
     case COLOR_RGB2XYZ:
